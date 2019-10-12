3d247dc6c2e9626269dc2fe35a2b25bbc16c68d8
bruno <brunohma@live.com>
2019-10-11 22:24:15 -0300

    pwIII-router / bruno e igor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { NovoProdutoComponent } from './novo-produto/novo-produto.component';                                                                                                                                                                                                                        
+import { Content } from '@angular/compiler/src/render3/r3_ast';
+import { ContentComponent } from './content/content.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  { 
+    path: 'produtos',
+    component: ProdutosComponent
+  },
+  {
+    path: 'novo-produto',
+    component: NovoProdutoComponent
+  },
+  {
+    path: '',
+    component: ContentComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
